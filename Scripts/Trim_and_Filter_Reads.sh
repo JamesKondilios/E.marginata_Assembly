ea59a16d5d2ac1292a96e7346a6bffcd826f8513
James Kondilios <u6046902@anu.edu.au>
2018-12-06 16:39:02 +1100

    Fixed new directorys in Trim_and_Filter_Reads.sh

--- Scripts/Trim_and_Filter_Reads.sh
+++ Scripts/Trim_and_Filter_Reads.sh
@@ -9,6 +9,6 @@
 #PBS -m abe
 #PBS -M  james.kondilios@anu.edu.au
 
-cat Ash_Emarginata_flowcell-2_SQK-LSK109_basecalled/workspace/*.fastq  | /home/801/jk9065/.local/bin/NanoFilt --headcrop 100 --tailcrop 100 -l 1000  > trimmed-reads.fastq #trim 100 bases off the start and end of each read
-trimmed-reads.fastq | /home/801/jk9065/.local/bin/NanoFilt -q 7 > trimmed-qualityfiltered-reads.fastq # filter out reads with q score < 7
-/home/801/jk9065/.local/bin/NanoPlot -t 1 --fastq trimmed-qualityfiltered-reads.fastq -o /NanoPlotTrim_Filtered 
+cat /g/data/xe2/projects/james_directory/e.marg_assembly/Data/Ash_Emarginata_flowcell-2_SQK-LSK109_basecalled/workspace/*.fastq  | /home/801/jk9065/.local/bin/NanoFilt --headcrop 100 --tailcrop 100 -l 1000  > trimmed-reads.fastq #trim 100 bases off the start and end of each read
+trimmed-reads.fastq | /home/801/jk9065/.local/bin/NanoFilt -q 7 > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq # filter out reads with q score < 7
+/home/801/jk9065/.local/bin/NanoPlot -t 1 --fastq /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq -o /g/data/xe2/projects/james_directory/e.marg_assembly/Data/NanoPlotTrim_Filtered 
