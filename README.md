fe64cafc3611a6eec2488a12d785aa5b658ef7e1
James Kondilios <u6046902@anu.edu.au>
2018-12-06 16:20:14 +1100

    Added outline to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# E.marginata_Assembly
+# E.marginata_Assembly #
+
+## Progress ##
+1. trimmed 100 bases off each side of all reads
+2. removed all reads with < 1000 bases
+3. removed all reads with a q score < 7 (after trimming so that average score is increased an d all the reads arent removed in this step)
+
+## TODO ##
+* read correction step (align all reads to all reads)
---
+++ Scripts/Trim_and_Filter_Reads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -P xe2
+#PBS -q normalbw
+#PBS -l walltime=24:00:00
+#PBS -l mem=16GB
+#PBS -l jobfs=100GB
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -m abe
+#PBS -M  james.kondilios@anu.edu.au
+
+cat Ash_Emarginata_flowcell-2_SQK-LSK109_basecalled/workspace/*.fastq  | /home/801/jk9065/.local/bin/NanoFilt --headcrop 100 --tailcrop 100 -l 1000  > trimmed-reads.fastq #trim 100 bases off the start and end of each read
+trimmed-reads.fastq | /home/801/jk9065/.local/bin/NanoFilt -q 7 > trimmed-qualityfiltered-reads.fastq # filter out reads with q score < 7
+NanoPlot -t 1 --fastq trimmed-qualityfiltered-reads.fastq -o /NanoPlotTrim_Filtered 
