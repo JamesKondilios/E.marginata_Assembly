0076746a0e2afdd50fe12c95d24ab9317e8977a7
James Kondilios <u6046902@anu.edu.au>
2018-12-10 21:40:13 +1100

    changed filtering parameters

--- Scripts/Trim_and_Filter_Reads.sh
+++ Scripts/Trim_and_Filter_Reads.sh
@@ -9,6 +9,6 @@
 #PBS -m abe
 #PBS -M  james.kondilios@anu.edu.au
 
-cat /g/data/xe2/projects/james_directory/e.marg_assembly/Data/Ash_Emarginata_flowcell-2_SQK-LSK109_basecalled/workspace/*.fastq  | /home/801/jk9065/.local/bin/NanoFilt --headcrop 100 --tailcrop 100 -l 1000 | /home/801/jk9065/.local/bin/NanoFilt --headcrop 0 --tailcrop 0 -l 1000 -q 7 > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq
+cat /g/data/xe2/projects/james_directory/e.marg_assembly/Data/Ash_Emarginata_flowcell-2_SQK-LSK109_basecalled/workspace/*.fastq  | /home/801/jk9065/.local/bin/NanoFilt --headcrop 100 --tailcrop 100 -l 1000 | /home/801/jk9065/.local/bin/NanoFilt -q 7 > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq
 
 /home/801/jk9065/.local/bin/NanoPlot -t 1 --fastq /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq -o /g/data/xe2/projects/james_directory/e.marg_assembly/Data/NanoPlotTrim_Filtered
