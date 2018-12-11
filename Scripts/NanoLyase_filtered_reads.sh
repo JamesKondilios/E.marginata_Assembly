811f20ab3f3aca72f624d454163db8e16f26b498
James Kondilios <u6046902@anu.edu.au>
2018-12-11 14:24:06 +1100

    added cat to first NanoLyase call

--- Scripts/NanoLyase_filtered_reads.sh
+++ Scripts/NanoLyase_filtered_reads.sh
@@ -9,6 +9,6 @@
 #PBS -m abe
 #PBS -M  james.kondilios@anu.edu.au
 
-/g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq | /home/801/jk9065/.local/bin/NanoLyse -r /g/data/xe2/projects/james_directory/e.marg_assembly/Data/DNA_CS.fasta > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads_withoutLambda.fastq;
+cat /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads.fastq | /home/801/jk9065/.local/bin/NanoLyse -r /g/data/xe2/projects/james_directory/e.marg_assembly/Data/DNA_CS.fasta > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads_withoutLambda.fastq;
 
 /home/801/jk9065/.local/bin/NanoPlot -t 5 --fastq /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads_withoutLambda.fastq -o /g/data/xe2/projects/james_directory/e.marg_assembly/Data/plot_trimmed-qualityfiltered-reads_withoutLambda
