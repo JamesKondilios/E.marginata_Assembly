8f4ead267f7bafa2cf9cfdbe548c46c8756116c8
James Kondilios <u6046902@anu.edu.au>
2018-12-11 13:26:15 +1100

    changed variable names in added more informative variable names to NanoLyase script

--- Scripts/NanoLyase_filtered_reads.sh
+++ Scripts/NanoLyase_filtered_reads.sh
@@ -9,6 +9,6 @@
 #PBS -m abe
 #PBS -M  james.kondilios@anu.edu.au
 
-trimmed-qualityfiltered-reads.fastq | /home/801/jk9065/.local/bin/NanoLyse -r /g/data/xe2/projects/james_directory/e.marg_assembly/Data/DNA_CS.fasta > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/ControlStrand_removaled_trimmed-qualityfiltered-reads.fastq;
+trimmed-qualityfiltered-reads.fastq | /home/801/jk9065/.local/bin/NanoLyse -r /g/data/xe2/projects/james_directory/e.marg_assembly/Data/DNA_CS.fasta > /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads_withoutLambda.fastq;
 
-/home/801/jk9065/.local/bin/NanoPlot -t 5 --fastq /g/data/xe2/projects/james_directory/e.marg_assembly/Data/ControlStrand_removed_trimmed-qualityfiltered-reads.fastq -o /g/data/xe2/projects/james_directory/e.marg_assembly/Data/plot_ControlStrandRemoved_reads -t 5
+/home/801/jk9065/.local/bin/NanoPlot -t 5 --fastq /g/data/xe2/projects/james_directory/e.marg_assembly/Data/trimmed-qualityfiltered-reads_withoutLambda.fastq -o /g/data/xe2/projects/james_directory/e.marg_assembly/Data/plot_trimmed-qualityfiltered-reads_withoutLambda
